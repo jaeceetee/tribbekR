0c1ec066ee41d070a497c3bef3a428d15dbe8e6d
Jai Cheon <jai.cheon@gmail.com>
2021-04-13 21:10:30 -0700

    update for channels

--- CapstoneProj/CapstoneProj/settings.py
+++ CapstoneProj/CapstoneProj/settings.py
@@ -130,12 +130,12 @@ STATIC_URL = '/static/'
 # Channels settings
 ASGI_APPLICATION = "CapstoneProj.asgi.application"
 CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': 'channels_redis.core.RedisChannelLayer',
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
-    }
+    },
 }
 
 if 'DATABASE_URL' in os.environ:
