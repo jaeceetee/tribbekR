b51f066558b68ec291256b75b12557eb45773a53
Jai Cheon <jai.cheon@gmail.com>
2021-04-13 21:17:03 -0700

    initial upload

--- CapstoneProj/CapstoneProj/settings.py
+++ CapstoneProj/CapstoneProj/settings.py
@@ -133,7 +133,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
+            "hosts": [os.environ.get('HEROKU_REDIS_ROSE_URL', 'redis://localhost:6379')],
         },
     },
 }
