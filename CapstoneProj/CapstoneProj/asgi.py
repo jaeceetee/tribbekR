e72e309531ec18b4ead089050604c386ba3cfb4e
Jai Cheon <jai.cheon@gmail.com>
2021-04-15 19:17:58 -0700

    update heroku

--- CapstoneProj/CapstoneProj/asgi.py
+++ CapstoneProj/CapstoneProj/asgi.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 """
 import os
 import sys
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(BASE_DIR)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'CapstonProj.CapstoneProj.settings'
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CapstonProj.CapstoneProj.settings")
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CapstonProj.CapstoneProj.settings")
 import django
 # os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
