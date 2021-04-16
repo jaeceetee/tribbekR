a73e06f6584143d9b395d0869accdb75625e1380
Jai Cheon <jai.cheon@gmail.com>
2021-04-15 19:41:50 -0700

    update heroku

--- CapstoneProj/CapstoneProj/settings.py
+++ CapstoneProj/CapstoneProj/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.contenttypes',
     'channels',
     'trivia',
     'Users',
     'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
