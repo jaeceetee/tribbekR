8ae1f344e64eb30f9e0a3d25e83c0303c82c0d40
Jai Cheon <jai.cheon@gmail.com>
2021-04-16 17:51:42 -0700

    removed key

--- CapstoneProj/settings.py
+++ CapstoneProj/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 
+from .secret_setting import *
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j7q%xmn+4#pl!n2=$=e42eix*%_dmfq(jd+9ro%y8j3i*-!0)6'
+# SECRET_KEY = secret_key_hidden
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
